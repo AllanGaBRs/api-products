cbf3ad253197496abed3fecf35d53d5e9040f74d
allangabr <allanmediogames@gmail.com>
2025-02-07 16:31:00 -0300

    configuring h2 database and test endpoint

---
+++ src/main/java/com/allan/produtosapi/model/Product.java
@@ -0,0 +1,52 @@
+package com.allan.produtosapi.model;
+
+
+public class Product {
+
+    private String id;
+    private String name;
+    private String desc;
+    private Double price;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", desc='" + desc + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
